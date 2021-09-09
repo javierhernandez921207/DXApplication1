eea9e78f0310b5f82d56786b24386e517816ecf5
Javier <jhcastrolil@gmail.com>
2021-09-09 11:23:40 -0400

    Add project files.

---
+++ DXApplication1.Module/BusinessObjects/Contact.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+
+namespace DXApplication1.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Contact : Person
+    {
+        public Contact(Session session) : base(session) { }
+        private string webPageAddress;
+        public string WebPageAddress
+        {
+            get { return webPageAddress; }
+            set { SetPropertyValue(nameof(WebPageAddress), ref webPageAddress, value); }
+        }
+        private string nickName;
+        public string NickName
+        {
+            get { return nickName; }
+            set { SetPropertyValue(nameof(NickName), ref nickName, value); }
+        }
+        private string spouseName;
+        public string SpouseName
+        {
+            get { return spouseName; }
+            set { SetPropertyValue(nameof(SpouseName), ref spouseName, value); }
+        }
+        private TitleOfCourtesy titleOfCourtesy;
+        public TitleOfCourtesy TitleOfCourtesy
+        {
+            get { return titleOfCourtesy; }
+            set { SetPropertyValue(nameof(TitleOfCourtesy), ref titleOfCourtesy, value); }
+        }
+        private DateTime anniversary;
+        public DateTime Anniversary
+        {
+            get { return anniversary; }
+            set { SetPropertyValue(nameof(Anniversary), ref anniversary, value); }
+        }
+        private string notes;
+        [Size(4096)]
+        public string Notes
+        {
+            get { return notes; }
+            set { SetPropertyValue(nameof(Notes), ref notes, value); }
+        }
+    }
+    public enum TitleOfCourtesy { Dr, Miss, Mr, Mrs, Ms };
+}
