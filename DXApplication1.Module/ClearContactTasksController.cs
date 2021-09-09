f1552968c54f77752370918a9705dd677d89e7b3
Javier <jhcastrolil@gmail.com>
2021-09-09 15:53:34 -0400

    Add a Simple Action

---
+++ DXApplication1.Module/ClearContactTasksController.cs
@@ -0,0 +1,64 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+using DXApplication1.Module.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DXApplication1.Module
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class ClearContactTasksController : ViewController
+    {
+        public ClearContactTasksController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetViewType = ViewType.DetailView;
+            TargetObjectType = typeof(Contact);
+
+            SimpleAction clearTasksAction = new SimpleAction(this, "ClearTaskAction", PredefinedCategory.View)
+            {
+                Caption = "Clear tasks",
+                ConfirmationMessage = "Are you sure you want to clear the Tasks list?",
+                ImageName = "Action_Clear"
+            };
+            clearTasksAction.Execute += ClearTasksAction_Execute;
+        }
+
+        private void ClearTasksAction_Execute(Object sender, SimpleActionExecuteEventArgs e)
+        {
+            while (((Contact)View.CurrentObject).Tasks.Count > 0)
+            {
+                ((Contact)View.CurrentObject).Tasks.Remove(((Contact)View.CurrentObject).Tasks[0]);
+            }
+            ObjectSpace.SetModified(View.CurrentObject);
+        }
+
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
